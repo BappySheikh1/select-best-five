38e7e34c7d6c72b8e0ec7c37d1f11640464c0ea0
Your Name Main <you@example.com>
2022-08-22 04:43:47 -0700

    modify added

---
+++ js/delegate.js
@@ -0,0 +1,20 @@
+
+// const buttonSelect=document.getElementsByClassName('button');
+// let count =0;
+
+// for(let i =0; i< buttonSelect.length;i++){
+//  const element=buttonSelect[i];
+ 
+//  element.addEventListener('click',function(event){
+//     count++
+//     console.log(count)
+//    const unOrdedList=document.getElementById('unOrded-list');
+//    const li=document.createElement('li');
+//    li.innerText=event.target.parentNode.parentNode.children[0].innerText;
+//    unOrdedList.appendChild(li);
+//     // this.disabled=true;
+   
+//  })
+// }
+
+
--- js/script.js
+++ js/script.js
@@ -16,9 +16,37 @@ function setTextValyeById(elementId,value){
     const element=document.getElementById(elementId);
     element.innerText=value;
 }
+// 
+
+
+// 
+
+const buttonSelect=document.getElementsByClassName('button');
+let count =0;
+
+for(let i =0; i< buttonSelect.length;i++){
+ const element=buttonSelect[i];
+ 
+ element.addEventListener('click',function(event){
+    count++
+    if(count > 5){
+        alert('5jon er beshi add deoya jabe nah')
+        return;
+    }
+   const unOrdedList=document.getElementById('unOrded-list');
+   const li=document.createElement('li');
+   li.innerText=event.target.parentNode.parentNode.children[0].innerText;
+   unOrdedList.appendChild(li);
+     this.disabled=true;
+   
+ })
+}
+
+// 
+
 document.getElementById('btn-calculate').addEventListener('click',function(){
     const perPlayerField=getInputValueById('perPlayer-field');
-    const totalPlayerCost=perPlayerField * 5;
+    const totalPlayerCost=perPlayerField * count;
     setTextValyeById('playerExpenses-total',totalPlayerCost);
 })
 
@@ -31,3 +59,5 @@ document.getElementById('calculate-total').addEventListener('click',function(){
     const totalCostCalculate = totalPlayerCost + managerTotalCost + coachTotalCost;
     setTextValyeById('total-cost',totalCostCalculate)
 })
+
+// 
